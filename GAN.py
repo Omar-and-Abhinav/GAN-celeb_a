e2378f5cb22670ebf2fddb5e284fa9fbea17718b
OAK-02 <omarbdllh1@gmail.com>
2020-12-02 16:31:19 +0300

    modification in the architecture of neural nets

--- GAN.py
+++ GAN.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from load_celeb_data import photo_dataset
-from tensorflow.keras.layers import Conv2D, MaxPool2D, UpSampling2D, Conv2DTranspose, BatchNormalization
+from tensorflow.keras.layers import Conv2D, MaxPool2D, UpSampling2D, Conv2DTranspose, BatchNormalization, LeakyReLU
 from tensorflow.keras import Input
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,7 +8,7 @@ from tensorflow import keras
 import os
 
 dataset = photo_dataset()
-dataset.load_data(1000)
+dataset.load_data(30000)
 #x_train, x_val, x_test = dataset.split_data(dataset.data, 0.8, 0.0)
 
 gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -56,7 +56,7 @@ class GAN(tf.keras.Model):
         images = tf.concat([generated_images, data], axis=0)
         labels = tf.concat(
             [tf.zeros(batch_size, 1), tf.ones(batch_size, 1)], axis = 0)
-        print(labels)
+        labels += 0.05 * tf.random.uniform(tf.shape(labels))
         with tf.GradientTape() as tape:
             predictions = self.discriminator(images)
             print("Shape:", predictions.shape, labels.shape, images.shape)
@@ -85,18 +85,21 @@ x = UpSampling2D((2,2))(x)
 x = Conv2DTranspose(24, (3, 3), activation = 'relu', name = 'covtr3')(x)
 x = BatchNormalization()(x)
 x = UpSampling2D((2,2))(x)
-gen_out = Conv2DTranspose(3, (10, 6), activation='sigmoid', name = 'covtr4')(x)
+gen_out = Conv2DTranspose(3, (10, 6), activation='tanh', name = 'covtr4')(x)
 
 generator = keras.Model(inputs = gen_inp, outputs = gen_out, name = 'Generator' )
 #generator.summary()
 
 # DISCRIMINATOR
 disc_inp = Input(shape = (109, 89, 3), name = 'disc_inp')
-x = Conv2D(32, (4, 4), strides = (3, 3), activation = 'relu', name = 'firstDiscLayer')(disc_inp)
+x = Conv2D(32, (4, 4), strides = (3, 3), name = 'firstDiscLayer')(disc_inp)
+x = LeakyReLU(alpha = 0.3)(x)
 x = BatchNormalization()(x)
-x = Conv2D(16, (3, 3), strides = (2, 2) , activation = 'relu', name='secondDiscLayer' )(x)
+x = Conv2D(16, (3, 3), strides = (2, 2) , name='secondDiscLayer' )(x)
+x = LeakyReLU(alpha = 0.3)(x)
 x = BatchNormalization()(x)
-x = Conv2D(8, (3, 3), strides = (2, 2), activation = 'relu', name = 'thirdDiscLayer')(x)
+x = Conv2D(8, (3, 3), strides = (2, 2), name = 'thirdDiscLayer')(x)
+x = LeakyReLU(alpha = 0.3)(x)
 x = BatchNormalization()(x)
 x = tf.keras.layers.Flatten()(x)
 disc_out = tf.keras.layers.Dense(1, activation = 'sigmoid')(x)
@@ -108,11 +111,37 @@ discriminator = keras.Model(inputs = disc_inp, outputs = disc_out, name = 'Discr
 
 gan = GAN(discriminator, generator, (9, 7, 8))
 gan.compile(
-    keras.optimizers.Adam(learning_rate = 0.00000000003),
-    keras.optimizers.Adam(learning_rate = 0.00000000003),
+    keras.optimizers.Adam(learning_rate = 0.003),
+    keras.optimizers.Adam(learning_rate = 0.003),
     discriminator_loss,
     generator_loss
 )
-gan.fit(dataset.data, epochs = 10, batch_size = 10)
+gan.fit(dataset.data/255, epochs = 10, batch_size = 10)
 
 
+def show_images(images, cols = 1, titles = None):
+    """From https://gist.github.com/soply/f3eec2e79c165e39c9d540e916142ae1
+    Display a list of images in a single figure with matplotlib.
+
+    Parameters
+    ---------
+    images: List of np.arrays compatible with plt.imshow.
+
+    cols (Default = 1): Number of columns in figure (number of rows is 
+                        set to np.ceil(n_images/float(cols))).
+
+    titles: List of titles corresponding to each image. Must have
+            the same length as titles.
+    """
+    assert((titles is None)or (len(images) == len(titles)))
+    n_images = len(images)
+    if titles is None: titles = ['Image (%d)' % i for i in range(1,n_images + 1)]
+    fig = plt.figure()
+    for n, (image, title) in enumerate(zip(images, titles)):
+        a = fig.add_subplot(cols, np.ceil(n_images/float(cols)), n + 1)
+        if image.ndim == 2:
+            plt.gray()
+        plt.imshow(image)
+        a.set_title(title)
+    fig.set_size_inches(np.array(fig.get_size_inches()) * n_images)
+    plt.show()
